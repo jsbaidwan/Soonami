de37c021ecabf61509b35a04d7d14ff5846c403f
jaspreet.singh <cgc.jsbaidwan@gmail.com>
2017-12-23 23:27:36 +0530

    Initializing onCreate method

--- app/src/main/java/com/example/jaspreetsingh/soonami/MainActivity.java
+++ app/src/main/java/com/example/jaspreetsingh/soonami/MainActivity.java
@@ -5,6 +5,7 @@ import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
 
+    // Initializing onCreate method
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
