b09642a3d2c7153ae2ea2605d656ebe689c4e428
jaspreet.singh <cgc.jsbaidwan@gmail.com>
2017-12-29 17:36:14 +0530

    Referring to the layout activity

--- app/src/main/java/com/example/jaspreetsingh/soonami/MainActivity.java
+++ app/src/main/java/com/example/jaspreetsingh/soonami/MainActivity.java
@@ -9,6 +9,7 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        // Refers to the activity_main layout
         setContentView(R.layout.activity_main);
     }
 }
